1b6ef3a2d35cd6a317ba7de5fec41992f23da592
Priit Meos <priitm@proekspert.ee>
2019-11-28 20:03:14 +0200

    Support multiple sensors

--- README.md
+++ README.md
@@ -5,14 +5,18 @@
 3. Run `npm install` in the repository's directory.
 4. Start the data API server using `node server.js`.
 5. Open browser to see the graph `http://localhost:8081/`
+  You can specify sensors in query parameters `http://localhost:8081/?sensor=Sensor1&sensor=Sensor2`
 
 ## REST API 
 
 /api/create
 Creates database
 
-/api/add?sensor=Sensor1&value=14.96
+/api/sensors/:sensor/points/add/:value
 Adds new measurement value with current date
+
+/api/sensors/:sensor/points
+Retrieve measurement points for sensor
 
 /api/points
 Retrieve all measurement points
--- server.js
+++ server.js
@@ -11,27 +11,42 @@ app.get('/api/create', function(request, result) {
 	result.send("Andmebaas loodud");
 });
 
-app.get('/api/points/dummy', function(request, result) {
-	result.send([
-		{date: "2019-10-11T00:01:01Z", temperature: 23},
-		{date: "2019-10-11T01:05:01Z", temperature: 25},
-		{date: "2019-10-11T01:25:01Z", temperature: 28},
-		{date: "2019-10-11T02:11:01Z", temperature: 24},
-		{date: "2019-10-11T22:17:01Z", temperature: 23.8},
-		{date: "2019-10-12T02:10:00Z", temperature: 22.1},
-	]);
-});
-
-app.get('/api/points', function(request, result) {
-	db.all('SELECT date, value temperature FROM measurement ORDER BY date', [], (err, rows) => {
+app.get('/api/sensors/:sensor/points', function(request, result) {
+	const sensor = request.params.sensor;
+	db.all('SELECT date, value temperature FROM measurement WHERE sensor=? ORDER BY date', [sensor], (err, rows) => {
 		result.send(rows);
 	});
 });
 
-app.get('/api/add', function(request, result) {
-	const sensor = request.query.sensor;
+app.get('/api/points', function(request, result) {
+	let sensors = request.query.sensor;
+	let sql;
+	if (sensors) {
+		if (Array.isArray(sensors)) {
+			sql = 'SELECT sensor, date, value FROM measurement WHERE sensor IN (' + sensors.map(x => '?').join(',') + ') ORDER BY date';
+		} else {
+			sql = 'SELECT sensor, date, value FROM measurement WHERE sensor = ? ORDER BY date';
+			sensors = [sensors];
+		}
+	} else {
+		sql = 'SELECT sensor, date, value FROM measurement ORDER BY date';
+		sensors = [];
+	}
+	const series = [];
+	db.all(sql, sensors, (err, rows) => {
+		rows.forEach(row => {
+			const elm = {date: row.date};
+			elm[row.sensor] = row.value;
+			series.push(elm);
+		});
+		result.send(series);
+	});
+});
+
+app.get('/api/sensors/:sensor/points/add/:value', function(request, result) {
+	const sensor = request.params.sensor;
 	const date = new Date().toJSON();
-	const value = request.query.value;
+	const value = request.params.value;
 	db.run('INSERT INTO measurement(sensor, date, value) VALUES(?, ?, ?)', [sensor, date, value], function(err) {
 		result.send("Lisatud kirje");
 	});
