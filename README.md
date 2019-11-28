06aaf2f901749fdcfa9267e7f377abf9ead72c60
Priit Meos <priitm@proekspert.ee>
2019-11-28 18:39:10 +0200

    Using SQLite database and amcharts as graph library

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 2. Clone this repository.
 3. Run `npm install` in the repository's directory.
 4. Start the data API server using `node server.js`.
+5. Open browser to see the graph `http://localhost:8081/`
+
+## REST API 
+
+/api/create
+Creates database
+
+/api/add?sensor=Sensor1&value=14.96
+Adds new measurement value with current date
+
+/api/points
+Retrieve all measurement points
--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-// Data API
+const sqlite3 = require('sqlite3');
+let db = new sqlite3.Database('data.db');
 
 var express = require("express");
 var app = express();
 
-app.get('server/points', function(request, result) {
-	console.log("Placeholder GET server/points");
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/create', function(request, result) {
+	db.run('CREATE TABLE measurement(sensor text, date text, value double)');
+	result.send("Andmebaas loodud");
 });
 
-app.get('server/point/last', function(request, result) {
-	console.log("Placeholder GET server/point/last");
+app.get('/api/points/dummy', function(request, result) {
+	result.send([
+		{date: "2019-10-11T00:01:01Z", temperature: 23},
+		{date: "2019-10-11T01:05:01Z", temperature: 25},
+		{date: "2019-10-11T01:25:01Z", temperature: 28},
+		{date: "2019-10-11T02:11:01Z", temperature: 24},
+		{date: "2019-10-11T22:17:01Z", temperature: 23.8},
+		{date: "2019-10-12T02:10:00Z", temperature: 22.1},
+	]);
 });
 
-app.put('server/point', function(request, result) {
-	console.log("Placeholder PUT server/point");
+app.get('/api/points', function(request, result) {
+	db.all('SELECT date, value temperature FROM measurement ORDER BY date', [], (err, rows) => {
+		result.send(rows);
+	});
+});
+
+app.get('/api/add', function(request, result) {
+	const sensor = request.query.sensor;
+	const date = new Date().toJSON();
+	const value = request.query.value;
+	db.run('INSERT INTO measurement(sensor, date, value) VALUES(?, ?, ?)', [sensor, date, value], function(err) {
+		result.send("Lisatud kirje");
+	});
 });
 
 var server = app.listen(8081, function() {
-	console.log("Töötan!");
+	console.log("Server töötab");
 }); 
