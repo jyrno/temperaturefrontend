0ec8a40f0272621c968f84772067960f79186cf9
Eric Johann Istal <erici@proekspert.ee>
2019-11-28 10:23:19 +0200

    Initial REST API endpoints

--- server.js
+++ server.js
@@ -2,6 +2,19 @@
 
 var express = require("express");
 var app = express();
+
+app.get('server/points', function(request, result) {
+	console.log("Placeholder GET server/points");
+});
+
+app.get('server/point/last', function(request, result) {
+	console.log("Placeholder GET server/point/last");
+});
+
+app.put('server/point', function(request, result) {
+	console.log("Placeholder PUT server/point");
+});
+
 var server = app.listen(8081, function() {
 	console.log("Töötan!");
 }); 
